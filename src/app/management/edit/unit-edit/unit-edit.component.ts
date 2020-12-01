90150b212d08659f54949647a175b613bf04b33c
mrhb <mmmhajjar83gmail.com>
2020-12-01 18:46:49 +0330

    some edit to show buildings

--- src/app/management/edit/unit-edit/unit-edit.component.ts
+++ src/app/management/edit/unit-edit/unit-edit.component.ts
@@ -4,6 +4,7 @@ import { UnitsService } from '../../services/units.service';
 import { Unit } from '../../services/unit';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-unit-edit',
   templateUrl: './unit-edit.component.html',
@@ -11,17 +12,40 @@ import { switchMap } from 'rxjs/operators';
 })
 export class UnitEditComponent implements OnInit {
   unit$: Observable<Unit>;
+  formGroup :FormGroup ; 
+
+  myGroup :FormGroup ; 
+
+titleAlert: string = 'This field is required';
+post: any = '';
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private service: UnitsService
+    // private service: UnitsService,
+    private formBuilder: FormBuilder
   ) { }
-  
+  step = 3;
+  onFormSubmit(): void {
+    console.log('Name:' + this.formGroup.get('name').value);
+} 
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
+
   ngOnInit(): void {
-    this.unit$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) =>
-      this.service.get(params.get('id')))
-      );
+    // this.unit$ = this.route.paramMap.pipe(
+    //   switchMap((params: ParamMap) =>
+    //   this.service.get(params.get('id')))
+    //   );
     }
 
 
@@ -32,5 +56,18 @@ export class UnitEditComponent implements OnInit {
       // Include a junk 'foo' property for fun.
       this.router.navigate(['/management/units']);
     }
+    resetForm() { 
+
+      // this.unit$ = this.route.paramMap.pipe(
+      //   switchMap((params: ParamMap) =>
+      //   this.service.get(params.get('id')))
+      //   );
+        
+      this.formGroup.reset({
+         name: "sdfv",
+         age: 20
+      });
+   } 
+
 
 }
