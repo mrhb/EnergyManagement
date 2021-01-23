6e604e386b2c4038e84622ca89173bcce621f4dc
mrhb <mmmhajjar83gmail.com>
2021-01-23 17:31:13 +0330

    chnage production setting

--- src/app/_base/service/model/rest-constants.ts
+++ src/app/_base/service/model/rest-constants.ts
@@ -1,6 +1,7 @@
 import {HttpHeaders} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
-export const GATEWAY_URL = 'http://194.5.179.45:3000';
+export const GATEWAY_URL =environment.API
 export const TOKEN_PREFIX = 'Bearer ';
 export const TOKEN_HEADER_NAME = 'Authorization';
 export const TOKEN_CACHE_KEY = 'TOKEN';
