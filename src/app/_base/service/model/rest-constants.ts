0380678c5042f1a79ccadbe3f18c7d23dabce456
Mohammad Javad Imani <mjimani.ir@gmail.com>
2021-01-25 08:36:48 +0330

    building create/update

--- src/app/_base/service/model/rest-constants.ts
+++ src/app/_base/service/model/rest-constants.ts
@@ -1,6 +1,7 @@
 import {HttpHeaders} from '@angular/common/http';
 
-export const GATEWAY_URL = 'http://194.5.179.45:3000';
+// export const GATEWAY_URL = 'http://194.5.179.45:3000';
+export const GATEWAY_URL = 'http://localhost:3000';
 export const TOKEN_PREFIX = 'Bearer ';
 export const TOKEN_HEADER_NAME = 'Authorization';
 export const TOKEN_CACHE_KEY = 'TOKEN';
