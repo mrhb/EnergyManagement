5a9d8ccd042633c83324f458c14e9a56325000fe
mrhb <mmmhajjar83gmail.com>
2021-02-20 18:29:40 +0330

    PowerReceiptService added

---
+++ src/app/main-modules/user/configuration/subscription/service/power-receipt.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {GeneralService} from '../../../../../_base/service/_service/general.service';
+import {HttpClient} from '@angular/common/http';
+import {GATEWAY_URL} from '../../../../../_base/service/model/rest-constants';
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class PowerReceiptService extends GeneralService {
+
+  constructor(public http: HttpClient) {
+    super(http);
+    this.prefixPath = GATEWAY_URL + '/api/power-receipt';
+  }
+
+  createReceipt(body: any, errorSelector?: string): Observable<any> {
+    return super.postCheckService('create', body, errorSelector);
+  }
+
+  getOneReceipt(param: {id: string}, errorSelector?: string): Observable<any> {
+    return super.getCheckService('get-one', errorSelector, param);
+  }
+
+  updateReceipt(param: {id: string}, body: any, errorSelector?: string): Observable<any> {
+    return super.putCheckService('update', body, errorSelector, param);
+  }
+
+  getReceiptList(param: any, body: any,  errorSelector?: string): Observable<any> {
+    return super.postCheckService('get-list-pageable-by-filter', body, errorSelector, param);
+  }
+
+  deleteReceipt(param: {id: string}, errorSelector?: string): Observable<any> {
+    return super.deleteCheckService('delete', errorSelector, param);
+  }
+}
