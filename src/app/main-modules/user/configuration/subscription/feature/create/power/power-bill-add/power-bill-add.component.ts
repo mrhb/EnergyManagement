4b57ad5daee3fb03cd75ce02d17d9605b1914a0f
KazemPourBadakhshan <k_pour@yahoo.com>
2021-02-21 18:24:20 +0330

    Add Bill power _1

--- src/app/main-modules/user/configuration/subscription/feature/create/power/power-bill-add/power-bill-add.component.ts
+++ src/app/main-modules/user/configuration/subscription/feature/create/power/power-bill-add/power-bill-add.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {PowerDto} from '../../../../model/power';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-power-bill-add',
   templateUrl: './power-bill-add.component.html',
+
   styleUrls: ['./power-bill-add.component.scss']
 })
 export class PowerBillAddComponent implements OnInit {
+
+  pageSize = 20;
+  pageIndex = 0;
+  length = -1;
+
+  touched = false;
+  edited = false;
+  powerId = '';
+  form: FormGroup;
+  powerDto = new PowerDto();
 
   constructor() { }
 
@@ -13,3 +26,5 @@ export class PowerBillAddComponent implements OnInit {
   }
 
 }
+
+
