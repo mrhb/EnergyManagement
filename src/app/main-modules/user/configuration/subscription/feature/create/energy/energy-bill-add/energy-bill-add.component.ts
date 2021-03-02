bbd4d1d734397c3174214517f73116ad82e70688
KazemPourBadakhshan <k_pour@yahoo.com>
2021-03-02 18:03:27 +0330

    data energy bill edited

--- src/app/main-modules/user/configuration/subscription/feature/create/energy/energy-bill-add/energy-bill-add.component.ts
+++ src/app/main-modules/user/configuration/subscription/feature/create/energy/energy-bill-add/energy-bill-add.component.ts
@@ -3,7 +3,7 @@
  * Email: k_pour@yahoo.com
  */
 
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {EnergyAllocation, EnergyBillDto,EnergyList} from '../../../../model/energy';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
@@ -11,19 +11,22 @@ import { MyPattern } from 'src/app/shared/tools/myPattern';
 import { EnergyReceiptService } from '../../../../service/energy-receipt.service';
 import { EnergyService } from '../../../../service/energy.service';
 import Notiflix from 'notiflix';
+import { Moment } from 'src/app/shared/tools/moment';
 declare var $: any;
 @Component({
   selector: 'app-Energy-bill-add',
   templateUrl: './energy-bill-add.component.html',
   styleUrls: ['./energy-bill-add.component.scss']
 })
-export class EnergyBillAddComponent implements OnInit {
+export class EnergyBillAddComponent implements OnInit , AfterViewInit{
   pageSize = 20;
   pageIndex = 0;
   length = -1;
   touched = false;
   edited = false;
   myPattern = MyPattern;
+  moment = Moment;
+
   energyId = '';
   energyList: EnergyList[] = [];
 
@@ -37,8 +40,59 @@ export class EnergyBillAddComponent implements OnInit {
     private  energyService: EnergyService,
     private  energyReceiptService: EnergyReceiptService
 ) { 
-
 }
+  // this.activatedRoute.queryParams.subscribe(params => {
+  //   console.log('params', params);
+  //   if (params.id) {
+  //     this.edited = true;
+  //     this.waterId = params.id;
+  //     this.getOneBill(params.id);
+  //   }
+  // });
+
+
+  ngAfterViewInit(): void {
+    this.jQueryDate();
+  }
+  
+  jQueryDate(): void {
+    setTimeout(e1 => {
+      $('#fromDate').MdPersianDateTimePicker({
+        Placement: 'bottom', // default is 'bottom'
+        Trigger: 'focus', // default is 'focus',
+        targetTextSelector: '#fromDate',
+        disableAfterToday: false,
+        disableBeforeToday: false,
+      }).on('change', (e) => {
+        this.energyBillDto.fromDate = this.moment.convertJaliliToIsoDate($(e.currentTarget).val());
+        if (this.energyBillDto.fromDate > this.energyBillDto.toDate) {
+          setTimeout(() => {
+            Notiflix.Notify.Failure('تاریخ شروع باید قبل از تاریخ پایان انتخاب شود');
+            this.energyBillDto.toDate = null;
+            $('#toDate').val(this.moment.getJaliliDateFromIso(this.energyBillDto.toDate));
+          }, 200);
+        }
+      });
+      $('#toDate').MdPersianDateTimePicker({
+        Placement: 'bottom', // default is 'bottom'
+        Trigger: 'focus', // default is 'focus',
+        targetTextSelector: '#toDate',
+        disableAfterToday: false,
+        disableBeforeToday: false,
+      }).on('change', (e) => {
+        this.energyBillDto.toDate = this.moment.convertJaliliToIsoDate($(e.currentTarget).val());
+        console.log('this.energyBillDto.toDate', this.energyBillDto.toDate);
+        if (this.energyBillDto.fromDate > this.energyBillDto.toDate) {
+          setTimeout(() => {
+            Notiflix.Notify.Failure('تاریخ وارده باید قبل از تاریخ شروع انتخاب شود');
+            this.energyBillDto.toDate = null;
+            $('#toDate').val(this.moment.getJaliliDateFromIso(this.energyBillDto.fromDate));
+          }, 200);
+        }
+      });
+    }, 100);
+  }
+
   ngOnInit(): void {
     this.form=this.formBuilder.group({
       energyCarrier: [''], //حامل انرژی 
@@ -59,8 +113,10 @@ export class EnergyBillAddComponent implements OnInit {
       .subscribe((res: any) => {
         if (res) {
           this.energyBillDto = res.data;
+          $('#fromDate').val(this.moment.getJaliliDateFromIso(this.energyBillDto.fromDate));
+          $('#toDate').val(this.moment.getJaliliDateFromIso(this.energyBillDto.toDate));
           this.energyAllocation= res.data.energySharing;
-          // this.setEnumUseType();
+            // this.setEnumUseType();
         }
       });
   }
