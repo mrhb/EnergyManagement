f41d09213aee9006c710a6faf5a6593232716110
mrhb <mmmhajjar83gmail.com>
2021-03-11 13:08:59 +0330

    قبض انرژی

---
+++ src/app/main-modules/user/configuration/subscription/model/energyEnum.ts
@@ -0,0 +1,14 @@
+//حامل انرژی
+export enum EnergyCarierEnum {
+  GAS = <any> 'گاز',
+  GASOLIN = <any> 'گازوزیل',
+  BENZIN = <any> 'بنزین',
+}
+// واحد
+export enum UnitInstrumentEnum {
+  LITER = <any> 'لیتر',
+  KWATT = <any> 'کیلووات',
+  METER3 = <any> 'مترمکعب',
+}
+
+
