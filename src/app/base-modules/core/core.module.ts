b11666a6d7b9178c9c25d325c24c11607037af7c
mrhb <mmmhajjar83gmail.com>
2021-03-16 16:32:04 +0330

    regionتمام شد

--- src/app/base-modules/core/core.module.ts
+++ src/app/base-modules/core/core.module.ts
@@ -5,9 +5,6 @@ import { CoreRoutingModule } from './core-routing.module';
 import {FooterComponent} from './footer/footer.component';
 import {HeaderComponent} from './header/header.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
-import { RegionComponent } from 'src/app/main-modules/user/configuration/region/feature/index/region.component';
-import { TreeViewComponent } from 'src/app/main-modules/user/configuration/region/feature/treeView/tree-view.component';
-
 
 @NgModule({
   declarations: [FooterComponent, HeaderComponent, SidebarComponent],
