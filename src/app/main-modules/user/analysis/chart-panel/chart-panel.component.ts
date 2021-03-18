51da72bd34068f4f9ac8147f4f42d384444051c1
mrhb <mmmhajjar83gmail.com>
2021-03-18 15:58:18 +0330

    chart category added

--- src/app/main-modules/user/analysis/chart-panel/chart-panel.component.ts
+++ src/app/main-modules/user/analysis/chart-panel/chart-panel.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
+import { CategoryEnum } from '../model/chartEnum';
 import { Backgrounds, Borders } from './ColorGenerator';
 
 @Component({
@@ -9,6 +10,7 @@ import { Backgrounds, Borders } from './ColorGenerator';
   styleUrls: ['./chart-panel.component.scss']
 })
 export class ChartPanelComponent implements OnInit {
+  @Input() category: CategoryEnum=CategoryEnum.LINE;
 
   lineChartData: ChartDataSets[] = [
     { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
@@ -20,96 +22,7 @@ export class ChartPanelComponent implements OnInit {
   lineChartOptions = {
     responsive: true,
   };
-  public ChartOptions: ChartOptions = {
-    responsive: true, 
-    title: {
-    display: false,
-    text: 'Unit 1'
-    },
-
-    tooltips: {
-        mode: 'index',
-        intersect: false,
-        callbacks: {
-          label: function(tooltipItem, data) {
-              return  Number(tooltipItem.yLabel).toFixed(2).replace(/./g, function(c, i, a) {
-                  return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "," + c : c;
-              });
-          }
-        }
-    },
-    scales: {
-          xAxes: [{
-                type: 'time',
-                time: {
-                      parser: 'MM/DD/YYYY HH:mm:ss',
-                      // round: 'minute',
-                      tooltipFormat: 'll HH:mm:ss'
-                  },
-                  scaleLabel: {
-                        display: true,
-                        labelString: ''
-                    },
-                    ticks: {
-                          maxRotation: 0
-                      }
-                  }],
-          yAxes: [{
-                scaleLabel: {
-                      display: true,
-                      //labelString: 'Power(Kw)'
-                  }
-              }]
-    },
-    plugins: {
-          zoom: {
-            // Container for pan options
-                pan: {
-                  // Boolean to enable panning
-                  enabled: true,
-
-                  // Panning directions. Remove the appropriate direction to disable
-                  // Eg. 'y' would only allow panning in the y direction
-                  // A function that is called as the user is panning and returns the
-                  // available directions can also be used:
-                  //   mode: function({ chart }) {
-                  //     return 'xy';
-                  //   },
-                  mode: 'x',
-
-                  rangeMin: {
-                    // Format of min pan range depends on scale type
-                    x: null,
-                    y: null
-                  },
-                  rangeMax: {
-                    // Format of max pan range depends on scale type
-                    x: null,
-                    y: null
-                  },
-
-                  // On category scale, factor of pan velocity
-                  speed: 20,
-
-                  // Minimal pan distance required before actually applying pan
-                  threshold: 10,
-
-                  // Function called while the user is panning
-                  onPan: function({chart}) { console.log(`I'm panning!!!`); },
-                  // Function called once panning is completed
-                  onPanComplete: function({chart}) { console.log(`I was panned!!!`); }
-                },
-                zoom: {
-                      enabled: true,
-                      // Enable drag-to-zoom behavior
-                      drag: false,
-                      mode: 'x',
-                      speed: 0.1
-                  }
-              }
-    }
-  };
-
+ 
   lineChartColors: Color[] = [
     {
        borderColor: Borders[1],
@@ -127,10 +40,26 @@ export class ChartPanelComponent implements OnInit {
 
   lineChartLegend = true;
   lineChartPlugins = [];
-  lineChartType = 'line';
-  constructor() { }
 
+
+  lineChartType: ChartType = 'bar';
+
+  constructor() { 
+  }
+  
   ngOnInit(): void {
+    switch (this.category.toString())
+    {
+      case 'LINE' :
+        this.lineChartType = 'line';
+        break;
+        
+        case 'BAR':
+          this.lineChartType = 'bar';
+          break;
+        case 'COMBAR':
+        break;
+    }
   }
 
 }
--- src/app/main-modules/user/analysis/model/chartEnum.ts
+++ src/app/main-modules/user/analysis/model/chartEnum.ts
@@ -1,3 +1,5 @@
+import { ChartOptions } from "chart.js";
+
 export enum PeriodEnum {
   MONTHLY = <any> 'ماهانه',
   DAILY = <any> 'روزانه',
@@ -12,3 +14,111 @@ export enum chartTypeEnum {
   LINEAR = <any> 'خطی',
   BAR = <any> 'میله ای',
 }
+
+export enum CategoryEnum {
+  LINE,//خطی
+  BAR,// میله ای ساده
+  COMBAR,//  میله ای تجمعی
+  PIE,// 
+}
+
+export const OPTIONS: ChartOptions = {
+  responsive: true, 
+  title: {
+  display: false,
+  text: 'Unit 1'
+  },
+  elements:{
+    point:{
+      radius:0
+    }
+  },
+  tooltips: {
+      mode: 'index',
+      intersect: false,
+      callbacks: {
+        label: function(tooltipItem, data) {
+            return  Number(tooltipItem.yLabel).toFixed(2).replace(/./g, function(c, i, a) {
+                return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "," + c : c;
+            });
+        }
+      }
+  },
+  scales: {
+        xAxes: [{
+              type: 'time',
+              time: {
+                    parser: 'MM/DD/YYYY HH:mm:ss',
+                    round: 'minute',
+                    tooltipFormat: 'll HH:mm'
+                  },
+                scaleLabel: {
+                      display: true,
+                      labelString: 'Time'
+                  },
+                  ticks: {
+                        maxRotation: 45
+                    }
+                }],
+                yAxes: [{
+                  scaleLabel: {
+                        display: true,
+                        //labelString: 'Power(Kw)'
+                        
+                    },
+                    ticks: {
+                        // suggestedMin: 0,    // minimum will be 0, unless there is a lower value.
+                        // OR //
+                        beginAtZero: true   // minimum value will be 0.
+                    }
+                }
+            ]
+  },
+  plugins: {
+        zoom: {
+          // Container for pan options
+              pan: {
+                // Boolean to enable panning
+                enabled: true,
+
+                // Panning directions. Remove the appropriate direction to disable
+                // Eg. 'y' would only allow panning in the y direction
+                // A function that is called as the user is panning and returns the
+                // available directions can also be used:
+                //   mode: function({ chart }) {
+                //     return 'xy';
+                //   },
+                mode: 'x',
+
+                rangeMin: {
+                  // Format of min pan range depends on scale type
+                  x: null,
+                  y: null
+                },
+                rangeMax: {
+                  // Format of max pan range depends on scale type
+                  x: null,
+                  y: null
+                },
+
+                // On category scale, factor of pan velocity
+                speed: 20,
+
+                // Minimal pan distance required before actually applying pan
+                threshold: 10,
+
+                // Function called while the user is panning
+                onPan: function({chart}) { console.log(`I'm panning!!!`); },
+                // Function called once panning is completed
+                onPanComplete: function({chart}) { console.log(`I was panned!!!`); }
+              },
+              zoom: {
+                    enabled: true,
+                    // Enable drag-to-zoom behavior
+                    drag: false,
+                    mode: 'x',
+                    speed: 0.1
+                  }
+            }
+  }
+};
