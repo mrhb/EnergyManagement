c15bfd4a7d77bb69205d9c9ee3466a49061bc2b4
mrhb <mmmhajjar83gmail.com>
2021-03-18 11:52:20 +0330

    add ng2-chartjs

--- src/app/shared/paginator/test-pagination/test-pagination.component.spec.ts
+++ src/app/shared/paginator/test-pagination/test-pagination.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TestPaginationComponent } from './test-pagination.component';
 
@@ -6,7 +6,7 @@ describe('TestPaginationComponent', () => {
   let component: TestPaginationComponent;
   let fixture: ComponentFixture<TestPaginationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TestPaginationComponent ]
     })
