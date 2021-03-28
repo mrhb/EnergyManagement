f93e43bcf8891bd7e4d13d769e0edd766c217815
mrhb <mmmhajjar83gmail.com>
2021-03-28 16:07:17 +0430

    sdf

--- src/app/main-modules/user/analysis/suscriptions/model/powerEnum.ts
+++ src/app/main-modules/user/analysis/suscriptions/model/powerEnum.ts
@@ -1,9 +1,9 @@
 // نوع تحلیل اشتراک گاز
 export enum PowerAnalysisTypeEnum {
   DEMAND_PENALTY = <any> 'جریمه دیماند',
-  DEMAND = <any> ' دیماند',
-  DEMAND_SUM = <any> ' مجموع دیماند',
+  DEMAND = <any> ' تحلیل دیماند',
+  DEMAND_SUM = <any> 'مجموع دیماند قراردادی',
   REACTIVE = <any> 'جریمه بار راکتیو',
-  TARIFF = <any> ' تعرفه',
-  VOLTAGE = <any> 'ولتاژ',
+  TARIFF = <any> ' تعرفه', //io عنوان تعرفه
+  VOLTAGE = <any> 'ولتاژ تغذیه',
   }
