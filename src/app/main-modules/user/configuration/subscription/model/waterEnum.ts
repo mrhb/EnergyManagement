066ffd76f3ecb90e621755472e627675891ab126
KazemPourBadakhshan <k_pour@yahoo.com>
2021-04-03 11:57:51 +0430

    water sharing comments is fixed

--- src/app/main-modules/user/configuration/subscription/model/waterEnum.ts
+++ src/app/main-modules/user/configuration/subscription/model/waterEnum.ts
@@ -1,9 +1,22 @@
 export enum UseTypeWater {
-  PUBLIC = <any> 'عمومی',
+  DOMESTIC = <any> 'آب و فاضلاب خانگی',
+  COMMUNAL = <any> 'آب و فاضلاب مصارف اشتراکی',
+  GENERAL = <any> 'مصارف عمومی',
+  FREE = <any> 'آب فاضلاب آزاد',
+  GREEN = <any> 'فضای سبز',
+  PRODUCTION = <any> 'تولیدی',
+  COMMERCIAL = <any> 'مصارف تجاری',
 }
-
+	 
 export enum UseCodeWaterEnum {
-  PUBLIC = <any> 'عمومی',
-  GOVERNMENT = <any> 'دولتی',
-  HOME = <any> 'خانگی',
+  RESIDENTIAL = <any> 'مسکونی',
+  PUBLIC_GOVERNMENTAL = <any> 'عمومی و دولتی',
+  EDUCATIONAL_RELIGIOUS_PLACES = <any> 'آموزشی و اماکن مذهبی',
+  COMMERCIAL = <any> 'تجاری',
+  INDUSTRIAL = <any> 'صنعتی',
+  FREE_BUILT = <any> 'آزاد و بنایی',
+  FREE_INDUSTRIAL_CONSTRUCTION = <any> 'آزاد و بنایی صنعتی',
+  FREE_COMMERCIAL_BUILDING = <any> 'آزاد و بنایی تجاری',
+  FREE_PUBLIC_CONSTRUCTION = <any> 'آزاد و بنایی عمومی',
+  OTHER = <any> 'سایر',
 }
