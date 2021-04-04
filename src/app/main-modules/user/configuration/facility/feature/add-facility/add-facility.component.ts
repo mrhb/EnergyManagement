9447e0a72e7a09afe291ebcbfd55606a9907e493
mrhb <mmmhajjar83gmail.com>
2021-04-04 17:19:09 +0430

    ویرایش تأسیسات

--- src/app/main-modules/user/configuration/facility/feature/add-facility/add-facility.component.ts
+++ src/app/main-modules/user/configuration/facility/feature/add-facility/add-facility.component.ts
@@ -9,6 +9,8 @@ import {Tools} from '../../../../../../shared/tools/tools';
 import {Moment} from '../../../../../../shared/tools/moment';
 import { FacilityUsageEnum } from '../../model/facilityEnum';
 import { UtilityTypeEnum } from '../../../building/model/useTypeEnum';
+import { RegionService } from '../../../region/service/region.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-add-facility',
@@ -27,10 +29,15 @@ export class AddFacilityComponent implements OnInit {
    bId: string;
    facilitySharingId: string;
    editedFacilityDto = new FacilityDto();
-  region: any;
+  edited: boolean=false;
+  router: any;
 
-  constructor(private formBuilder: FormBuilder,
-              private facilityService: FacilityService) {
+  constructor(
+    private stateService:RegionService,
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private facilityService: FacilityService
+    ) {
     // this.facilityDto.constructionYear = this.moment.convertIsoToJDateFa(new Date().toISOString()).split('/')[0];
     // console.log('this.facilityDto.constructionYear', this.facilityDto.constructionYear.split('/'));
     this.form = this.formBuilder.group({
@@ -40,19 +47,30 @@ export class AddFacilityComponent implements OnInit {
       explanation: [''],//توضیحات
       address: [''], //آدرس
     });
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      console.log('params', params);
+      if (params.id) {
+        this.edited = true;
+        this.bId= params.id;
+        this.getOne(params.id);
+      }
+    });
+  }
+
+  getOne(gId): void {
+    this.facilityService.getOneFacility({id: gId})
+      .subscribe((res: any) => {
+        if (res) {
+          this.facilityDto = res.data;
+        }
+      });
   }
 
   ngOnInit(): void {
-    console.log('if (this.editedFacilityDto)', this.editedFacilityDto);
-    console.log('this.facilitySharingId', this.facilitySharingId);
-    console.log('this.bId   +', this.bId);
-    if (this.facilitySharingId) {
-      this.facilityDto.facilitySharingId = this.facilitySharingId;
-
-      if (this.editedFacilityDto.facilitySharingId) {
-        this.facilityDto = this.editedFacilityDto;
-      }
-    }
+    this.stateService.regionId.subscribe(reg=>{
+      this.facilityDto.regionId=reg;
+    });
   }
 
   updateFacility(): void {
@@ -66,12 +84,16 @@ export class AddFacilityComponent implements OnInit {
     }
     console.log('this.facilityDto', this.facilityDto);
 
-    if (Tools.isNullOrUndefined(this.bId)) {
+    if (!this.edited) {
       this.facilityService.createFacility(this.facilityDto)
         .subscribe((res: any) => {
           if (res) {
             if (res.data) {
+              this.bId=res.data;
+              this.edited=true;
               Notiflix.Notify.Success('افزودن تآسیس با موفقیت انجام شد.');
+              this.router.navigate(['/index/user/configuration/facilityList']).then();
+
             }
           }
           console.log('facilityService res', res);
@@ -87,14 +109,5 @@ export class AddFacilityComponent implements OnInit {
           console.log('facilityService res', res);
         });
     }
-  }
-
-  getRegion($event): void {
-    // if (this.endActiveStep < 1) {
-    //   this.endActiveStep = 1;
-    // }
-     this.region = $event;
-     this.facilityDto.regionId = this.region.regionId;
-
   }
 }
