a7e49451b55dfd89426dcca28dd6ef5869189399
mrhb <mmmhajjar83gmail.com>
2021-04-07 11:51:34 +0430

    جابجایی فایل آبو هوا و اقلیم

--- src/app/main-modules/user/configuration/climate/model/climate.ts
+++ src/app/main-modules/user/configuration/climate/model/climate.ts
@@ -12,31 +12,7 @@ export class ClimateDto {
   dominantThermalReq: string; // نیاز غالب حرارتی
   energyDegree: string;  // درجه انرژی
 }
-//  اطلاعات آب و هوا (ماهانه)
-export class WeatherListDto {
-  month: string; // ماه
-  tempMaxMean: string;// میانگین حداکثر دما
-  tempMinMean: string;// میانگین حداقل دما
-  tempMean: string; //میانگین دما
-  HumidMaxMean: string;// میانگین حداکثر رطوبت
-  lowHumidMean: string;// میانگین حداقل رطوبت
-  HumidMean: string; // میانگین رطوبت
-  sunRadMean: string; //میانگین تابش خورشید
-  windMean: string;// میانگین سرعت باد 
-}
 
-  //  اطلاعات آب و هوا (روزانه)    
-export class WeatherDto {
-  forDate: string; // از تاریخ 
-  tempMax: string; // حداکثر دما روزانه
-  tempMin: string; // حداقل دما روزانه
-  tempAvg: string; // میانگین دما روزانه
-  humidityMin: string; // حداقل رطوبت روزانه
-  humidityMax: string; //حداکثر رطوبت روزانه
-  humidityAvg: string; //میانگین رطوبت روزانه
-  sunRad: string; //تابش
-  wind: string;//سرعت باد
-}
   //  اطلاعات آب و هوا و اقلیم (کلی)    
 export class ClimateList {
   province: ProvinceEnum;// استان  
--- src/app/main-modules/user/configuration/climate/model/weather.ts
+++ src/app/main-modules/user/configuration/climate/model/weather.ts
@@ -1,13 +1,25 @@
+//  اطلاعات آب و هوا (ماهانه)
+export class WeatherListDto {
+  month: string; // ماه
+  tempMaxMean: string;// میانگین حداکثر دما
+  tempMinMean: string;// میانگین حداقل دما
+  tempMean: string; //میانگین دما
+  HumidMaxMean: string;// میانگین حداکثر رطوبت
+  lowHumidMean: string;// میانگین حداقل رطوبت
+  HumidMean: string; // میانگین رطوبت
+  sunRadMean: string; //میانگین تابش خورشید
+  windMean: string;// میانگین سرعت باد 
+}
 
-export class WeatherList {
-    id: string;
-    month: string;// ماه
-    highDegMean: string;// میانگین حداکثر دما
-    lowDegMean: string;// میانگین حداقل دما
-    DegMean: string; //میانگین دما
-    highHumidMean: string;// میانگین حداکثر رطوبت
-    lowHumidMean: string;// میانگین حداقل رطوبت
-    HumidMean: string; // میانگین رطوبت
-    sunRadMean: string; //میانگین تابش خورشید
-    windMean: string;// میانگین سرعت باد     
-  }
+  //  اطلاعات آب و هوا (روزانه)    
+export class WeatherDto {
+  forDate: string; // از تاریخ 
+  tempMax: string; // حداکثر دما روزانه
+  tempMin: string; // حداقل دما روزانه
+  tempAvg: string; // میانگین دما روزانه
+  humidityMin: string; // حداقل رطوبت روزانه
+  humidityMax: string; //حداکثر رطوبت روزانه
+  humidityAvg: string; //میانگین رطوبت روزانه
+  sunRad: string; //تابش
+  wind: string;//سرعت باد
+}
