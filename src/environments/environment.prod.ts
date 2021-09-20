50e04abfee0223f456422fce7b9acab8e3a9c35d
mrhb <mmmhajjar83gmail.com>
2021-09-20 20:17:19 +0430

    migrate to linux

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API: 'http://185.252.29.76:3000'
+  // API: 'http://185.252.29.76:3000'
+  API: 'http://localhost:3000'
 };
---
+++ src/environments/environment.prodLinux.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  API: 'http://194.5.205.150:3000'
+};
